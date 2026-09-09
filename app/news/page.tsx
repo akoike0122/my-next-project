7d00624af81ae30ceb2ed946c4088eb4a545cd45
akoike <akoike@ewmjapan.com>
2026-09-09 16:04:36 +0900

    ニュースにISRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,7 +3,7 @@ import NewsList from "@/app/_components/NewsList";
 import SearchField from "@/app/_components/SerchField";
 import Pagination from "@/app/_components/Pagination";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList();
